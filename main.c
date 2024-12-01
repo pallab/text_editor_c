2481fae8580ffb37145865b138f449b3b50cbf6a
Pallab Kalita <13886153+pallab@users.noreply.github.com>
2024-12-01 16:01:49 +0530

    append buffer

--- main.c
+++ main.c
@@ -95,6 +95,19 @@ struct abuf {
 
 #define ABUF_INIT { NULL, 0}
 
+void abAppend(struct abuf *ab, const char *s, int len){
+	char *new = realloc(ab->b, ab->len + len);
+	if (new == NULL) return;
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len = ab->len + len;
+}
+
+void abFree(struct abuf *ab) {
+	free(ab->b);
+	ab->len = 0
+}
+
 /*** output */
 
 void editorDrawRows() {
